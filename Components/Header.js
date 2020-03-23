3733e54b7b45e5d479e29a8b0f8100fb086ac764
Ernest Robin-Pezant <yoamon51@live.fr>
2020-03-23 14:42:45 +0100

    color.style2020

--- Components/Header.js
+++ Components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
 import { View , Text, Image, TouchableOpacity } from 'react-native';
 
+
 export class Header extends React.Component{
 
     render(){
         return ( 
-            <View style={{ background: this.props.style.background  /*backgroundColor:"#d32f2f"*/ ,flexDirection: 'row', height: HEADER_HEIGHT, justifyContent: 'space-between' }}>
+            <View style={{ backgroundColor : this.props.style.backgroundColor, /*backgroundColor:"#d32f2f"*/ flexDirection: 'row', height: HEADER_HEIGHT, justifyContent: 'space-between' }}>
                 <Text style={{ color : '#fff1f1',fontSize: 20, fontWeight: 'bold', textAlignVertical: 'center' }}>Muscu APP</Text>
                 <TouchableOpacity style={{ marginRight: 10 }} onPress={() => self.addSession()}>
                     <Image style={{ height: HEADER_HEIGHT - 20, width: HEADER_HEIGHT - 20, marginTop: 10 }} source={require('../ico/plus_ico.png')} />
--- Components/StyleElements.js
+++ Components/StyleElements.js
@@ -1,13 +1,15 @@
 import React from 'react'; 
-import StyleSheet from 'react-native'; 
+import {StyleSheet} from 'react-native'; 
 
 export const styles = StyleSheet.create({
 
     Header : {
-        background : '#d32f2f',
-        color : '#fff1f1'
+        backgroundColor : '#d32f2f',
+        color : '#fff1f1',
         
     }
 
 })
+
+
 
