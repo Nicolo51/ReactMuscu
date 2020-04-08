8b46c71b65fa4da5facb7eba4af84587b33968b8
Nicolas Descotes <nicolas.descotes@viacesi.fr>
2020-04-08 17:50:34 +0200

    merde

--- Components/Header.js
+++ Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import { View , Text, Image, TouchableOpacity, Dimensions } from 'react-native';
+import Images from '../index/';
 
 const WIDTH = Dimensions.get('window').width - 40;
 const HEADER_HEIGHT = 50;
@@ -10,8 +11,8 @@ export class Header extends React.Component{
     render(){
         return ( 
             <View style={{backgroundColor: this.props.style.backgroundColor, flexDirection: 'row', height: HEADER_HEIGHT, justifyContent: 'space-between' }}>
-                <Text style={{ color : this.props.style.color ,fontSize: 20, fontWeight: 'bold', textAlignVertical: 'center' }}>Muscu APP</Text>
-                <TouchableOpacity style={{ marginRight: 10 }} onPress={() => this.props.add}>
+                <Text style={{ color : this.props.style.color ,fontSize: 20, fontWeight: 'bold', textAlignVertical: 'center' }}>this.prop.tabName</Text>
+                <TouchableOpacity style={{ marginRight: 10 }} onPress={() => this.props.onButtonPress}>
                     <Image style={{ height: HEADER_HEIGHT - 20, width: HEADER_HEIGHT - 20, marginTop: 10 }} source={require('../ico/white_plus_ico.png')} />
                 </TouchableOpacity>
             </View>
