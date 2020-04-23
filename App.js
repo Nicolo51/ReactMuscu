9c7037e2d0d4441d910b584a3824d58bcee28b3b
Nicolas Descotes <nicolas.descotes@viacesi.fr>
2020-04-23 16:22:44 +0200

    MErge

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import SessionsList from './Components/SessionsList.js';
-import TrainSession from './Components/TrainSession.js'; 
-
+import SetsList from './Components/SetsList.js'; 
+import StatsExo from './Components/StatsExo.js';
 
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { CreateSet } from './Components/CreateSet.js';
 
-import StatsExo from './Components/StatsExo.js';
 
 
 navigateToScreen = (app, screen, option) => {
@@ -18,18 +18,43 @@ navigateToScreen = (app, screen, option) => {
   app.props.navigation.navigate(screen, option);
 }
 
+Save = async (name, json) => {
+    const JSONstring = JSON.stringify(json);
+    await AsyncStorage.setItem(name, JSONstring);
+    console.log(JSONstring + ": as been saved !"); 
+}
+
+Load = async (name) => {
+  try {
+    const value = await AsyncStorage.getItem(name);
+    if (value !== null) {
+        console.log(value + "as been loaded !"); 
+        let output = JSON.parse(value);
+        return output;
+    }
+  } catch (error) {
+      console.log('Something went wrong : ' + error);
+  }
+}
+
 const AppNavigator = createStackNavigator({ //declare all the screen possible of the stack navigator 
   SessionsList: { screen: SessionsList }, // <Name to call>: { screen: <imported Component from above> } 
-  TrainSession: { screen: TrainSession }, 
-  CreateSet : { screen: CreateSet },
+  SetsList: { screen: SetsList }, 
+  CreateSet : { screen: CreateSet }, 
   StatsExo: { screen: StatsExo},
 },
   {
-    initialRouteName: 'StatsExo', // What screen the app start at
+    initialRouteName: 'SessionsList', // What screen the app start at
   });
 
 export default createAppContainer(AppNavigator); // create the stack navigator
-//ceci est un com
-// JE suis sur la branch de dev de Nico
-//Com d'ernest
-//coucou
+
+/*
+[
+  {
+    key: 
+    name: 
+    Exercices[]: 
+  }
+]
+*/
