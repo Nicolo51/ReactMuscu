a6d7dbd01cac4db8c4a3720219bc21308b6feb46
unknown <ernest.robinpezant@viacesi.fr>
2020-08-14 14:54:33 +0200

    Note

--- Components/ExoNote.js
+++ Components/ExoNote.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AsyncStorage, TouchableOpacity, StyleSheet, View, ScrollView, Image, Text, Dimensions } from 'react-native';
+import { AsyncStorage, TouchableOpacity, StyleSheet, View, ScrollView, Image, Text, Dimensions, TextInput } from 'react-native';
 import Header from './Header.js'; 
 import Images from '../index.js';
 import StyleElements from './StyleElements.js';
@@ -31,11 +31,15 @@ export class ExoNote extends React.Component {
             
             <ScrollView vertical={true} style={{flex: 1, flexDirection: "column"}}>
                 <Image style={{ height: 300, width:  250, marginLeft: 50, marginTop: 5}} source={Images.getImage('bibi')} />
-                    <View style={{height: 250, width:  300, marginTop: 50, marginLeft:25}}>
-                        <Text style={{textAlign: 'center'}}>
-                                    Note
-                        </Text>
-                    </View>
+                    <View style={{height: 250, width:  300, marginTop: 10, marginLeft:25}}>
+                         <TextInput
+                                underlineColorAndroid="transparent"
+                                placeholder="Some notes for this set bro ?"
+                                placeholderTextColor="grey"
+                                numberOfLines={20}
+                                multiline={true}
+                                />
+                            </View>
             </ScrollView>
 
 
